e852632c8fdc8ab91d0cccc44d80ac08b34c68e0
Dmitry-coder <57069404+Dmitry-coder@users.noreply.github.com>
2021-09-08 17:41:13 +0300

    first version
    отсутствует синхронизация потоков (оператор lock или класс monitor). будет дорабатываться

---
+++ mpp_lab_1/mpp_lab_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace mpp_lab_1
+{
+	class Program
+	{
+		static void procID()
+		{
+			Console.WriteLine("текущий id = " + Thread.CurrentThread.ManagedThreadId);
+			Thread.Sleep(100);
+		}
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello World!");
+			TaskQueue taskQueue = new TaskQueue(3);
+			Console.WriteLine("текущий id = " + Thread.CurrentThread.ManagedThreadId);
+			for (int i = 0; i < 3; i++)
+			{
+				taskQueue.EnqueueTask(procID);
+			}
+			Console.ReadLine();
+		}
+	}
+}
---
+++ mpp_lab_1/mpp_lab_1/TaskQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace mpp_lab_1
+{
+    public delegate void TaskDelegate();
+    class TaskQueue
+	{
+		private BlockingCollection<TaskDelegate> FuncQueue = new BlockingCollection<TaskDelegate>(new ConcurrentQueue<TaskDelegate>());
+		private int queueCount;
+
+        public TaskQueue(int queueCount)
+        {
+            this.queueCount = queueCount;
+            for (int i = 0; i < queueCount; i++)
+            {
+                var thread = new Thread(threadWork) { IsBackground = true }; ;
+                thread.Start();
+            }
+        }
+
+        public void threadWork()
+        {
+            while (true)
+            {
+                var task = FuncQueue.Take();
+                try
+                {
+                    task();
+                }
+                catch (ThreadStateException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (ThreadAbortException ex)
+                {
+                    Thread.ResetAbort();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            this.FuncQueue.Add(task);
+        }
+    }
+}
