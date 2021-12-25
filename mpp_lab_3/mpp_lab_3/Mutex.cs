fd4eb49c7f8d6f7943c9baf03333c42921b0f47a
Dmitry-coder <57069404+Dmitry-coder@users.noreply.github.com>
2021-12-25 15:09:26 +0300

    Add files via upload

---
+++ mpp_lab_3/mpp_lab_3/Mutex.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+
+namespace mpp_lab_3
+{
+    public class Mutex
+    {
+        private int curId = -1;
+
+        public void Lock()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;//Получает уникальный идентификатор текущего управляемого потока.
+            while (Interlocked.CompareExchange(ref this.curId, id, -1) != -1)//Сравнивает два значения на равенство и, если они равны, заменяет первое значение.
+                                                                             //1.Пункт назначения, значение которого сравнивается comparandи, возможно, заменяется.
+                                                                             //2.Значение, заменяющее целевое значение, если сравнение приводит к равенству.
+                                                                             //3.Значение, которое сравнивается со значением в location1.
+            {
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Unlock()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            Interlocked.CompareExchange(ref this.curId, -1, id);
+        }
+    }
+}
---
+++ mpp_lab_5/mpp_lab_5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+
+namespace mpp_lab_5
+{
+    class Program
+    {
+        static void main(String dir)
+        {
+            var assemblyLoader = new AssemblyLoader(dir);
+            assemblyLoader.ShowPublicTypes();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                main("C:\\mpp_lab_3.dll");
+            }
+            else
+            {
+                if (File.Exists(args[0]))
+                {
+                    main(args[0]);
+                }
+                else
+                {
+                    Console.WriteLine("Fail");
+                }
+            }
+        }
+    }
+
+    class AssemblyLoader
+    {
+        public Assembly Assembly { get; set; }
+
+        public AssemblyLoader(string assemblyPath)
+        {
+            try
+            {
+                Assembly = Assembly.LoadFrom(assemblyPath);//загрузка сборки в проект
+                Console.WriteLine("Assembly loaded successfully.");
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Load error: " + e.Message);
+            }
+        }
+
+        public void ShowPublicTypes()
+        {
+            var types = Assembly
+                .GetTypes()
+                .Where(t => t.IsPublic)
+                .OrderBy(t => t.Namespace)
+                .ThenBy(t => t.Name);
+            foreach (var type in types)
+            {
+                Console.WriteLine(type.FullName);
+            }
+        }
+    }
+}
