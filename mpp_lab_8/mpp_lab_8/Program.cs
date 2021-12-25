219ad17cad97aed6a141ec30bbccb9d4556e4c29
Dmitry-coder <57069404+Dmitry-coder@users.noreply.github.com>
2021-12-25 15:10:29 +0300

    Add files via upload

---
+++ mpp_lab_8/mpp_lab_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace SPP8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\mpp_lab_8.dll";
+            Assembly assembly = Assembly.LoadFrom(path);
+            var types = assembly.GetTypes().Where(t => t.IsPublic && t.IsDefined(typeof(ExportClass), false));//фолс не будет дочерних
+            Console.WriteLine("ExportClass:");
+            foreach (var type in types)
+            {
+                Console.WriteLine(type.FullName);
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]//ограниченя для класса
+    public class ExportClass : Attribute { }//наследуем чтобы экспорт юзать как атрибут
+
+    [ExportClass]
+    public class PublicClass { }
+    [ExportClass]
+    internal class InternalClass { }
+}
